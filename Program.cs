f61e6db52108078fd3ff034ed8795fb9d918bcf5
Eddie <edrismoavenghafouri@gmail.com>
2024-11-03 20:15:35 +0330

    Add extension method for configuring GoogleCalendarSettings

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
-
-builder.Services.Configure<GoogleCalendarSettings>(builder.Configuration.GetSection(nameof(GoogleCalendarSettings)));
-
+builder.RegisterSettingService();
 builder.Services.AddScoped<IGoogleCalendarService, GoogleCalendarService>();
 builder.Services.AddScoped<IOAuthService, OAuthService>();
 
@@ -13,3 +10,13 @@ app.UseRouting();
 app.MapControllers();
 
 app.Run();
+
+
+public static class AddSetting
+{
+    public static void RegisterSettingService(this WebApplicationBuilder builder)
+    {
+        builder.Services.Configure<GoogleCalendarSettings>
+            (builder.Configuration.GetSection(nameof(GoogleCalendarSettings)));
+    }
+}
